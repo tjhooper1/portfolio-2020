d0dc61de319304e11df5defa2ccbe6161b7ffcab
thomas hooper <tomh386@gmail.com>
2020-07-26 00:08:58 -0400

    feat: finished

--- script.js
+++ script.js
@@ -1,2 +1,22 @@
 let themeDot = document.getElementsByClassName('theme-dot')
-console.log(themeDot)
+let themeStyle = document.getElementById('theme-style')
+
+const setTheme = (mode) => {
+    themeStyle.href = `${mode}.css`
+    localStorage.setItem('theme', mode)
+}
+
+let theme = localStorage.getItem('theme')
+
+if (theme === null) {
+    setTheme('default')
+} else {
+    setTheme(theme)
+}
+
+for (let i = 0; i < themeDot.length; i++) {
+    themeDot[i].addEventListener('click', () => {
+        let mode = themeDot[i].dataset.mode 
+        setTheme(mode)
+    })
+}
